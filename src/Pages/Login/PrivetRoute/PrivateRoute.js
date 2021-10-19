759e9a21f5b5baafc539646fc0a8bf1d67914d52
emonislam079 <emonislam079@gmail.com>
2021-10-19 19:04:14 +0600

    diploy

--- src/Pages/Login/PrivetRoute/PrivateRoute.js
+++ src/Pages/Login/PrivetRoute/PrivateRoute.js
@@ -6,7 +6,7 @@ import useAuth from '../../../Hooks/useAuth';
 const PrivateRoute = ({ children, ...rest }) => {
     const { user, isLoading } = useAuth();
     if (isLoading) {
-        return <Spinner animation="border" variant="danger" />
+        return <Spinner animation="border" variant="warning" className="mt-5 pt-5" />
     }
     return (
         <Route
